minor fix, deleted softmax function

models/multiclass_classifier.py
@@ -27,6 +27,6 @@ class MulticlassClassifier(nn.Module):
         x = self.relu(x)
         x = self.dropout(x)
         x = self.fc3(x)
-        x = self.softmax(x)
+       ## x = self.softmax(x)
         return x
 