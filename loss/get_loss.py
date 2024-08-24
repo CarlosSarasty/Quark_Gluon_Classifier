2700ea60490a07838557f03574de53f6fb8225e7
Carlos Sarasty <sarastce@mail.uc.edu>
2024-08-24 12:35:32 -0400

    Adding focal loss and EMD2 loss

--- loss/get_loss.py
+++ loss/get_loss.py
@@ -1,8 +1,15 @@
-def get_loss(func, **params):
-  if func == 'focal_loss':
-    from loss.loss import Focal_loss
-    return Focal_loss
+def get_loss(**params):
+   
+    func = params.get('loss_function')
 
-  else:
-    from torch import nn
-    return getattr(nn.modules.loss, func)(**params)
+    if func == 'emd2_loss':
+        from loss.loss import emd2_loss
+        return emd2_loss
+  
+    if func == 'focal_loss':
+        from loss.focal_loss import Focal_loss
+        return Focal_loss
+
+    else:
+        from torch import nn
+        return getattr(nn.modules.loss, func)(**params)
